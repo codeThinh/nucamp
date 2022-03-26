d9efeaec09165974cc8ad6cefcf7deebe146fdeb
codeThinh <codethinh@gmail.com>
2022-03-26 00:00:46 -0700

    react week 1

---
+++ 3-React/i-heart-react/src/App.js
@@ -0,0 +1,29 @@
+import HeartLogo from './heart.svg';
+import './App.css';
+
+const message = "cool cud"
+
+const Heart = (props) => {
+  return (
+    <div className="heart">
+      <img className="heart-img" src={HeartLogo} alt="heart" />
+      <p className="heart-message">{props.msg}</p>
+    </div>
+  )
+}
+const Header = (props) => {
+  return (
+    <div className="App-header">I ❤️ React</div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Heart msg={message} />
+    </>
+  );
+}
+
+export default App;
