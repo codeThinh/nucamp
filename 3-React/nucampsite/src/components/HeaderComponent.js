f31bff574f35906241ba3b99acb07ab61c9b41d0
codeThinh <codethinh@gmail.com>
2022-04-11 22:49:36 -0700

    react header and footer

---
+++ 3-React/nucampsite/src/components/HeaderComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className="row">
+                            <h1>NuCamp</h1>
+                            <h2>a better way to camp</h2>
+                        </div>
+                    </div>
+                </Jumbotron>
+                <Navbar dark sticky="top">
+                <div className="container">
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+                </div>
+                </Navbar>
+            </React.Fragment>
+        )
+    }
+}
+
--- 3-React/nucampsite/src/index.js
+++ 3-React/nucampsite/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster'
-import 'typeface-lobster'
+import 'typeface-open-sans'
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(
