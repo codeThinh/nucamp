a5801a6be9ff4eea8ada2207df39e6c9331e5c09
codeThinh <codethinh@gmail.com>
2022-04-26 21:35:04 -0700

    fetch server

--- 3-React/nucampsite/src/components/ContactComponent.js
+++ 3-React/nucampsite/src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import {Breadcrumb, BreadcrumbItem, Button, Label, Col, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import React, {Component} from 'react';
-import {Control, LocalForm, Errors} from 'react-redux-form';
+import {Control, Form, Errors, actions} from 'react-redux-form';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -30,10 +30,11 @@ export default class Contact extends Component {
     };
   }
 
-  handleSubmit = values => {
-    console.log('Current state is: ' + JSON.stringify(values));
-    alert('Current state is: ' + JSON.stringify(values));
-  };
+  handleSubmit(values) {
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
+    this.props.resetFeedbackForm();
+  }
 
   render() {
     return (
@@ -78,7 +79,7 @@ export default class Contact extends Component {
             <hr />
           </div>
           <div className="col-md-10">
-            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+            <Form model="feedbackForm" onSubmit={values => this.handleSubmit(values)}>
               <Row className="form-group">
                 <Label htmlFor="firstName" md={2}>
                   First Name
@@ -236,7 +237,7 @@ export default class Contact extends Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -8,7 +8,8 @@ import About from './AboutComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Contact from './ContactComponent';
-import {addComment, fetchCampsites} from '../redux/ActionCreators';
+import {addComment, fetchCampsites, fetchComments, fetchPromotions} from '../redux/ActionCreators';
+import {actions} from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -22,11 +23,16 @@ const mapStateToProps = state => {
 const mapDispatchToProps = {
   addComment: (campsiteId, rating, author, text) => addComment(campsiteId, rating, author, text),
   fetchCampsites: () => fetchCampsites(),
+  resetFeedbackForm: () => actions.reset('feedbackForm'),
+  fetchComments: () => fetchComments(),
+  fetchPromotions: () => fetchPromotions(),
 };
 
 class Main extends Component {
   componentDidMount() {
     this.props.fetchCampsites();
+    this.props.fetchComments();
+    this.props.fetchPromotions();
   }
 
   render() {
@@ -36,7 +42,9 @@ class Main extends Component {
           campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
           campsitesLoading={this.props.campsites.isLoading}
           campsitesErrMess={this.props.campsites.errMess}
-          promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+          promotion={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
+          promotionLoading={this.props.promotions.isLoading}
+          promotionErrMess={this.props.promotions.errMess}
           partner={this.props.partners.filter(partner => partner.featured)[0]}
         />
       );
@@ -52,9 +60,10 @@ class Main extends Component {
           }
           isLoading={this.props.campsites.isLoading}
           errMess={this.props.campsites.errMess}
-          comments={this.props.comments.filter(
+          comments={this.props.comments.comments.filter(
             comment => comment.campsiteId === +match.params.campsiteId
           )}
+          commentsErrMess={this.props.comments.ErrMess}
           addComment={this.props.addComment}
         />
       );
@@ -71,7 +80,11 @@ class Main extends Component {
             path="/directory"
             render={() => <Directory campsites={this.props.campsites} />}
           />
-          <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/contactus"
+            render={() => <Contact resetFeedbackFrom={this.props.resetFeedbackForm} />}
+          />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Redirect to="/home" />
         </Switch>
