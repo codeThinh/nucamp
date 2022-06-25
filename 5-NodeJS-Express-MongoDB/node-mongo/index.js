e57426da87556a1cb5c6230b864f940a5fddb70c
codeThinh <codethinh@gmail.com>
2022-06-25 08:05:29 -0700

    node promises

--- 5-NodeJS-Express-MongoDB/node-mongo/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongo/index.js
@@ -1,35 +1,71 @@
 const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert").strict;
+const dbops = require("./operations");
 
 const url = "mongodb://localhost:27017/";
 const dbname = "nucampsite";
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  assert.strictEqual(err, null);
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected correctly to server");
 
-  console.log("Connected correctly to server");
+    const db = client.db(dbname);
 
-  const db = client.db(dbname);
+    db.dropCollection("campsites")
+      .then((result) => {
+        console.log("Dropped Collection:", result);
+      })
+      .catch((err) => console.log("No collection to drop."));
 
-  db.dropCollection("campsites", (err, result) => {
-    assert.strictEqual(err, null);
-    console.log("Dropped Collection", result);
+    dbops
+      .insertDocument(
+        db,
+        { name: "Breadcrumb Trail Campground", description: "Test" },
+        "campsites"
+      )
 
-    const collection = db.collection("campsites");
-
-    collection.insertOne(
-      { name: "Breadcrumb Trail Campground", description: "Test" },
-      (err, result) => {
-        assert.strictEqual(err, null);
+      .then((result) => {
         console.log("Insert Document:", result.ops);
 
-        collection.find().toArray((err, docs) => {
-          assert.strictEqual(err, null);
-          console.log("Found Documents:", docs);
+        return dbops.findDocuments(db, "campsites");
+      })
 
-          client.close();
-        });
-      }
-    );
-  });
-});
+      .then((docs) => {
+        console.log("Found Documents:", docs);
+
+        return dbops.updateDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          { description: "Updated Test Description" },
+          "campsites"
+        );
+      })
+
+      .then((result) => {
+        console.log("Updated Document Count:", result.result.nModified);
+
+        return dbops.findDocuments(db, "campsites");
+      })
+
+      .then((docs) => {
+        console.log("Found Documents:", docs);
+
+        return dbops.removeDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          "campsites"
+        );
+      })
+
+      .then((result) => {
+        console.log("Deleted Document Count:", result.deletedCount);
+
+        return client.close();
+      })
+
+      .catch((err) => {
+        console.log(err);
+        client.close();
+      });
+  })
+
+  .catch((err) => console.log(err));
